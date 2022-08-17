656681eb95e608b115a621d143f731f0e62cc00c
Johannes Arndt <75816216+johannesarndt@users.noreply.github.com>
2022-08-17 21:40:22 +0200

    tests for finding the best gui library

--- src/main.cpp
+++ src/main.cpp
@@ -1,143 +1,191 @@
 #include <Arduino.h>
 
-#include <TFT_eSPI.h> // Hardware-specific library
-#include <SPI.h>
 
-TFT_eSPI tft = TFT_eSPI();                   // Invoke custom library with default width and height
+#include "FS.h"
+#include "..\.pio\libdeps\pico\TFT_eSPI\examples\GUI Widgets\Buttons\Button_demo\Free_Fonts.h" // Include the header file attached to this sketch
 
-//TFT_eSPI tft = TFT_eSPI(240, 320);       // Could invoke custom library declaring width and height
+#include <TFT_eSPI.h>              // Hardware-specific library
+#include <TFT_eWidget.h>           // Widget library
 
-unsigned long targetTime = 0;
-byte red = 31;
-byte green = 0;
-byte blue = 0;
-byte state = 0;
-unsigned int colour = red << 11; // Colour order is RGB 5+6+5 bits each
+TFT_eSPI tft = TFT_eSPI();         // Invoke custom library
 
-void rainbow_fill();
+#define CALIBRATION_FILE "/TouchCalData1"
+#define REPEAT_CAL false
 
-void setup(void) {
-  Serial.begin(9600);
-  tft.init();
+ButtonWidget btnL = ButtonWidget(&tft);
+ButtonWidget btnR = ButtonWidget(&tft);
+
+#define BUTTON_W 100
+#define BUTTON_H 50
+
+// Create an array of button instances to use in for() loops
+// This is more useful where large numbers of buttons are employed
+ButtonWidget* btn[] = {&btnL , &btnR};;
+uint8_t buttonCount = sizeof(btn) / sizeof(btn[0]);
+
+void btnL_pressAction(void)
+{
+  if (btnL.justPressed()) {
+    Serial.println("Left button just pressed");
+    btnL.drawSmoothButton(true);
+  }
+}
+
+void btnL_releaseAction(void)
+{
+  static uint32_t waitTime = 1000;
+  if (btnL.justReleased()) {
+    Serial.println("Left button just released");
+    btnL.drawSmoothButton(false);
+    btnL.setReleaseTime(millis());
+    waitTime = 10000;
+  }
+  else {
+    if (millis() - btnL.getReleaseTime() >= waitTime) {
+      waitTime = 1000;
+      btnL.setReleaseTime(millis());
+      btnL.drawSmoothButton(!btnL.getState());
+    }
+  }
+}
+
+void btnR_pressAction(void)
+{
+  if (btnR.justPressed()) {
+    btnR.drawSmoothButton(!btnR.getState(), 3, TFT_BLACK, btnR.getState() ? "OFF" : "ON");
+    Serial.print("Button toggled: ");
+    if (btnR.getState()) Serial.println("ON");
+    else  Serial.println("OFF");
+    btnR.setPressTime(millis());
+  }
+
+  // if button pressed for more than 1 sec...
+  if (millis() - btnR.getPressTime() >= 1000) {
+    Serial.println("Stop pressing my buttton.......");
+  }
+  else Serial.println("Right button is being pressed");
+}
+
+void btnR_releaseAction(void)
+{
+  // Not action
+}
+
+void initButtons() {
+  uint16_t x = (tft.width() - BUTTON_W) / 2;
+  uint16_t y = tft.height() / 2 - BUTTON_H - 10;
+  btnL.initButtonUL(x, y, BUTTON_W, BUTTON_H, TFT_WHITE, TFT_RED, TFT_BLACK, "Button", 1);
+  btnL.setPressAction(btnL_pressAction);
+  btnL.setReleaseAction(btnL_releaseAction);
+  btnL.drawSmoothButton(false, 3, TFT_BLACK); // 3 is outline width, TFT_BLACK is the surrounding background colour for anti-aliasing
+
+  y = tft.height() / 2 + 10;
+  btnR.initButtonUL(x, y, BUTTON_W, BUTTON_H, TFT_WHITE, TFT_BLACK, TFT_GREEN, "OFF", 1);
+  btnR.setPressAction(btnR_pressAction);
+  //btnR.setReleaseAction(btnR_releaseAction);
+  btnR.drawSmoothButton(false, 3, TFT_BLACK); // 3 is outline width, TFT_BLACK is the surrounding background colour for anti-aliasing
+}
+
+void touch_calibrate()
+{
+  uint16_t calData[5];
+  uint8_t calDataOK = 0;
+
+  // check file system exists
+  if (!LittleFS.begin()) {
+    Serial.println("Formating file system");
+    LittleFS.format();
+    LittleFS.begin();
+  }
+
+  // check if calibration file exists and size is correct
+  if (LittleFS.exists(CALIBRATION_FILE)) {
+    if (REPEAT_CAL)
+    {
+      // Delete if we want to re-calibrate
+      LittleFS.remove(CALIBRATION_FILE);
+    }
+    else
+    {
+      File f = LittleFS.open(CALIBRATION_FILE, "r");
+      if (f) {
+        if (f.readBytes((char *)calData, 14) == 14)
+          calDataOK = 1;
+        f.close();
+      }
+    }
+  }
+
+  if (calDataOK && !REPEAT_CAL) {
+    // calibration data valid
+    tft.setTouch(calData);
+  } else {
+    // data not valid so recalibrate
+    tft.fillScreen(TFT_BLACK);
+    tft.setCursor(20, 0);
+    tft.setTextFont(2);
+    tft.setTextSize(1);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+
+    tft.println("Touch corners as indicated");
+
+    tft.setTextFont(1);
+    tft.println();
+
+    if (REPEAT_CAL) {
+      tft.setTextColor(TFT_RED, TFT_BLACK);
+      tft.println("Set REPEAT_CAL to false to stop this running again!");
+    }
+
+    tft.calibrateTouch(calData, TFT_MAGENTA, TFT_BLACK, 15);
+
+    tft.setTextColor(TFT_GREEN, TFT_BLACK);
+    tft.println("Calibration complete!");
+
+    // store data
+    File f = LittleFS.open(CALIBRATION_FILE, "w");
+    if (f) {
+      f.write((const unsigned char *)calData, 14);
+      f.close();
+    }
+  }
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  tft.begin();
   tft.setRotation(2);
   tft.fillScreen(TFT_BLACK);
+  tft.setFreeFont(FF18);
 
-  targetTime = millis() + 1000;
+  // Calibrate the touch screen and retrieve the scaling factors
+  touch_calibrate();
+  initButtons();
 }
 
 void loop() {
+  static uint32_t scanTime = millis();
+  uint16_t t_x = 9999, t_y = 9999; // To store the touch coordinates
 
-  if (targetTime < millis()) {
-    targetTime = millis() + 10000;
-
-    rainbow_fill(); // Fill the screen with rainbow colours
-
-    // The standard AdaFruit font still works as before
-    tft.setTextColor(TFT_BLACK); // Background is not defined so it is transparent
- 
-    tft.setCursor (60, 5);
-    tft.setTextFont(0);        // Select font 0 which is the Adafruit font
-    tft.print("Original Adafruit font!");
-
-    //tft.drawString("Original Adafruit font!",60,5,1); 
-
-    // The new larger fonts do not need to use the .setCursor call, coords are embedded
-    tft.setTextColor(TFT_BLACK); // Do not plot the background colour
-    // Overlay the black text on top of the rainbow plot (the advantage of not drawing the backgorund colour!)
-    tft.drawCentreString("Font size 2", 120, 14, 2); // Draw text centre at position 120, 14 using font 2
-    tft.drawCentreString("Font size 4", 120, 30, 4); // Draw text centre at position 120, 30 using font 4
-    tft.drawCentreString("12.34", 120, 54, 6);       // Draw text centre at position 120, 54 using font 6
-
-    tft.drawCentreString("12.34 is in font size 6", 120, 92, 2); // Draw text centre at position 120, 92 using font 2
-    // Note the x position is the top of the font!
-
-    // draw a floating point number
-    float pi = 3.14159; // Value to print
-    int precision = 3;  // Number of digits after decimal point
-    int xpos = 90;      // x position
-    int ypos = 110;     // y position
-    int font = 2;       // font number 2
-    xpos += tft.drawFloat(pi, precision, xpos, ypos, font); // Draw rounded number and return new xpos delta for next print position
-    tft.drawString(" is pi", xpos, ypos, font);             // Continue printing from new x position
-
-    tft.setTextSize(1);           // We are using a size multiplier of 1
-
-    tft.setTextColor(TFT_BLACK);  // Set text colour to black, no background (so transparent)
-
-    tft.setCursor(36, 150, 4);    // Set cursor to x = 36, y = 150 and use font 4
-    tft.println("Transparent...");  // As we use println, the cursor moves to the next line
-
-    tft.setCursor(30, 175);    // Set cursor to x = 30, y = 175
-    tft.setTextColor(TFT_WHITE, TFT_BLACK);  // Set text colour to white and background to black
-    tft.println("White on black");
-
-    tft.setTextFont(4);        // Select font 4 without moving cursor
-    tft.setCursor(50, 210);    // Set cursor to x = 50, y = 210 without changing the font
-    tft.setTextColor(TFT_WHITE);
-    // By using #TFT print we can use all the formatting features like printing HEX
-    tft.print(57005, HEX);    // Cursor does no move to next line
-    tft.println(48879, HEX);  // print and move cursor to next line
-
-    tft.setTextColor(TFT_GREEN, TFT_BLACK); // This time we will use green text on a black background
-    tft.setTextFont(2); // Select font 2
-    //Text will wrap to the next line if needed, by luck it breaks the lines at spaces!
-    tft.println(" Ode to a Small Lump of Green Putty I Found in My Armpit One Midsummer Morning ");
-  }
-}
-
-// Fill screen with a rainbow pattern
-void rainbow_fill()
-{
-  // The colours and state are not initialised so the start colour changes each time the funtion is called
-  
-  for (int i = 319; i > 0; i--) {
-    // Draw a vertical line 1 pixel wide in the selected colour
-    tft.drawFastHLine(0, i, tft.width(), colour); // in this example tft.width() returns the pixel width of the display
-    // This is a "state machine" that ramps up/down the colour brightnesses in sequence
-    switch (state) {
-      case 0:
-        green ++;
-        if (green == 64) {
-          green = 63;
-          state = 1;
+  // Scan keys every 50ms at most
+  if (millis() - scanTime >= 50) {
+    // Pressed will be set true if there is a valid touch on the screen
+    bool pressed = tft.getTouch(&t_x, &t_y);
+    scanTime = millis();
+    for (uint8_t b = 0; b < buttonCount; b++) {
+      if (pressed) {
+        if (btn[b]->contains(t_x, t_y)) {
+          btn[b]->press(true);
+          btn[b]->pressAction();
         }
-        break;
-      case 1:
-        red--;
-        if (red == 255) {
-          red = 0;
-          state = 2;
-        }
-        break;
-      case 2:
-        blue ++;
-        if (blue == 32) {
-          blue = 31;
-          state = 3;
-        }
-        break;
-      case 3:
-        green --;
-        if (green == 255) {
-          green = 0;
-          state = 4;
-        }
-        break;
-      case 4:
-        red ++;
-        if (red == 32) {
-          red = 31;
-          state = 5;
-        }
-        break;
-      case 5:
-        blue --;
-        if (blue == 255) {
-          blue = 0;
-          state = 0;
-        }
-        break;
+      }
+      else {
+        btn[b]->press(false);
+        btn[b]->releaseAction();
+      }
     }
-    colour = red << 11 | green << 5 | blue;
   }
+
 }
+
