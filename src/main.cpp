a910e037b028f08bfab04d4ce2a79d1afa2cdf44
Johannes Arndt <75816216+johannesarndt@users.noreply.github.com>
2022-12-31 15:07:00 +0100

    first screen of tutorial

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,13 @@ static const uint16_t screenHeight = 480;
 static lv_disp_draw_buf_t draw_buf;
 static lv_color_t buf[ screenWidth * 10 ];
 
+//lv_obj_t * scr1 = lv_obj_create(NULL);
+//lv_obj_t * scr2 = lv_obj_create(NULL);
+
+lv_obj_t * obj1;
+void secondscreen();
+void firstscreen();
+
 TFT_eSPI tft = TFT_eSPI(screenWidth, screenHeight); /* TFT instance */
 
 #if LV_USE_LOG != 0
@@ -80,35 +87,20 @@ void my_touchpad_read( lv_indev_drv_t * indev_driver, lv_indev_data_t * data )
     }
 }
 
-static void btn_event_cb(lv_event_t * e)
+
+
+/*void lv_example_get_started_2(void)
 {
-    lv_event_code_t code = lv_event_get_code(e);
-    lv_obj_t * btn = lv_event_get_target(e);
-    if(code == LV_EVENT_CLICKED) {
-        static uint8_t cnt = 0;
-        cnt++;
+    lv_obj_t * btn = lv_btn_create(lv_scr_act());    
+    //lv_obj_set_pos(btn, 10, 10);                           
+    /*lv_obj_align( btn, LV_ALIGN_CENTER, 0, 0 );
+    lv_obj_set_size(btn, 120, 50);                         
+    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);          
 
-        /*Get the first child of the button which is the label and change its text*/
-        lv_obj_t * label = lv_obj_get_child(btn, 0);
-        lv_label_set_text_fmt(label, "Button: %d", cnt);
-    }
-}
-
-/**
- * Create a button with a label and react on click event.
- */
-void lv_example_get_started_2(void)
-{
-    lv_obj_t * btn = lv_btn_create(lv_scr_act());     /*Add a button the current screen*/
-    //lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
-    lv_obj_align( btn, LV_ALIGN_CENTER, 0, 0 );
-    lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
-    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);           /*Assign a callback to the button*/
-
-    lv_obj_t * label = lv_label_create(btn);          /*Add a label to the button*/
-    lv_label_set_text(label, "Button");                     /*Set the labels text*/
+    lv_obj_t * label = lv_label_create(btn);          
+    lv_label_set_text(label, "Button");                    
     lv_obj_center(label);
-}
+}*/
 
 void touch_calibrate()
 {
@@ -175,6 +167,78 @@ void touch_calibrate()
   }
 }
 
+static void btn_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * btn = lv_event_get_target(e);
+    if(code == LV_EVENT_CLICKED) {
+        secondscreen();
+    }
+}
+
+static void btn_event_cb2(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * btn = lv_event_get_target(e);
+    if(code == LV_EVENT_CLICKED) {
+        firstscreen();
+    }
+}
+
+void firstscreen(){
+  
+  //obj1 = lv_obj_create(lv_scr_act());
+  //lv_obj_set_size(obj1, 320, 480);
+  
+
+    lv_obj_t * btn = lv_btn_create(lv_scr_act());     /*Add a button the current screen*/
+    //lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
+    lv_obj_align( btn, LV_ALIGN_CENTER, 0, 165 );
+    lv_obj_set_size(btn, 220, 80);                          /*Set its size*/
+    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL);           /*Assign a callback to the button*/
+
+    lv_obj_t * label = lv_label_create(btn);          /*Add a label to the button*/
+    lv_label_set_text(label, "Start");                     /*Set the labels text*/
+    lv_obj_set_style_text_font(label, &lv_font_montserrat_24, 0);
+    lv_obj_center(label);
+
+    lv_obj_t * label1 = lv_label_create(lv_scr_act());          /*Add a label to the button*/
+    lv_label_set_text(label1, "open_honey_fill");
+    lv_obj_set_style_text_font(label1, &lv_font_montserrat_26, 0);
+    lv_obj_align( label1, LV_ALIGN_CENTER, 0, -185 );
+
+    lv_obj_t * label2 = lv_label_create(lv_scr_act());          /*Add a label to the button*/
+    lv_label_set_text(label2, "Version 0.0.1");
+    lv_obj_align( label2, LV_ALIGN_CENTER, 0, -155 );
+
+    lv_obj_t * label3 = lv_label_create(lv_scr_act());          /*Add a label to the button*/
+    lv_label_set_long_mode(label3, LV_LABEL_LONG_WRAP);
+    lv_label_set_text(label3, "Lorem ipsum dolor sit amet, consetetur sadipscing elitr," 
+    "sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,"
+    "sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. "
+    "Stet clita kasd gubergren, no sea takimata sanctus est");
+    lv_obj_set_width(label3, 280); 
+    lv_obj_set_style_text_font(label3, &lv_font_montserrat_16, 0);
+    lv_obj_align( label3, LV_ALIGN_CENTER, 0, -15);
+}
+
+void secondscreen(){
+  lv_obj_t * obj2;
+  obj2 = lv_obj_create(lv_scr_act());
+  lv_obj_set_size(obj2, 320, 480);
+
+  lv_obj_t * btn = lv_btn_create(obj2);     /*Add a button the current screen*/
+    //lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
+    lv_obj_align( btn, LV_ALIGN_CENTER, 0, 0 );
+    lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
+    lv_obj_add_event_cb(btn, btn_event_cb2, LV_EVENT_ALL, NULL);           /*Assign a callback to the button*/
+
+    lv_obj_t * label = lv_label_create(btn);          /*Add a label to the button*/
+    lv_label_set_text(label, "2");                     /*Set the labels text*/
+    lv_obj_center(label);
+}
+
+
 
 
 
@@ -227,8 +291,9 @@ void setup()
    /* lv_obj_t *label = lv_label_create( lv_scr_act() );
     lv_label_set_text( label, LVGL_Arduino.c_str() );
     lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );*/
-    lv_example_get_started_2();
 
+    //lv_example_get_started_2();
+    firstscreen();
     Serial.println( "Setup done" );
 }
 
@@ -237,8 +302,8 @@ void loop()
   //Serial.println("tick_thread0");
     lv_task_handler(); /* let the GUI do its work */
 
-     delay(10);   /*Sleep for 5 millisecond*/
-    lv_tick_inc(10); 
+     delay(5);   /*Sleep for 5 millisecond*/
+    lv_tick_inc(5); 
     //Serial.println("tick_thread");
 }
 
